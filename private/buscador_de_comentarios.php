2172e1ef6365d2045f0c9cb4dea110d192aeba66
Dalbizu <70915352+Dalbizu@users.noreply.github.com>
2021-07-31 19:07:47 +0200

    El select ya coge las asignaturas de la base de datos

--- private/buscador_de_comentarios.php
+++ private/buscador_de_comentarios.php
@@ -11,7 +11,7 @@ foreach ($asignaturas as $asignatura) {
 	echo"<option value='".$asignatura["id_asignatura"]."'>".$asignatura["nombre_asignatura"]."</option>";
 };
 
-echo"<option>yeah</option>";
+// echo "<option>yeah</option>";
 
 $conexion->close();
 
