b207ae25176fab6e2e5138b00de9b56e4fcb194f
Dalbizu <70915352+Dalbizu@users.noreply.github.com>
2021-08-25 17:28:19 +0200

    Añadido boton de ver respuesta
    Sale el div correspondiente de manera correcta.
    Falta añadir la info

--- private/contenido_usuario_logeado.php
+++ private/contenido_usuario_logeado.php
@@ -4,6 +4,14 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="../maqueta/css/css_contenido.css">
+
+	<script src="../maqueta/js/jquery-1.9.1.js"></script>
+
+	<script>
+		function ver_respuestas() {
+			$("#respuestas").toggle();
+		}
+	</script>
 </head>
 
 <body>
@@ -15,12 +23,20 @@
 	include("conexion_foro.php");
 
 	// $sql="SELECT * FROM  comentarios";
-	$sql= "SELECT * FROM  comentarios INNER JOIN usuarios USING(id_usuario) ORDER BY id_comentario DESC LIMIT 10";
+	$sql = "SELECT * FROM  comentarios INNER JOIN usuarios USING(id_usuario) ORDER BY id_comentario DESC LIMIT 10";
 
 	$filas = $conexion->query($sql);
 
 	foreach ($filas as $fila) {
 		echo '
+
+		<script>
+      		function ver_respuestas' . $fila["id_comentario"] . '() 
+			{
+        		$("#respuestas' . $fila["id_comentario"] . '").toggle();
+      		}
+    	</script>
+
 		<div class="comentario" id="comentario_' . $fila["id_comentario"] . '">
 			<table>
 				<tr class="tabla_encabezado">
@@ -38,12 +54,22 @@
 					<td>fecha: ' . $fila["fecha_comentario"] . '</td>
 				</tr>
 				<tr class="tabla_respuesta">
-					<td colspan="3">	
-
+					<td>	
 				  	<form action="nueva_respuesta.php" method="GET">
     					<input name="id_comentario" type="hidden" value="' . $fila["id_comentario"] . '">
     					<input type="submit" value="Responder">
 					</form>
+					</td>
+
+					<td>
+
+					<button id="ver_respuestas" onclick="ver_respuestas' . $fila["id_comentario"] . '();">Ver respuestas</button>
+
+					</td>
+				</tr>
+				<tr id="respuestas' . $fila["id_comentario"] . '" style="display: none">
+					<td colspan="3">
+					<div id="">Respuesta</div>
 					</td>
 				</tr>
 			</table>
@@ -51,4 +77,3 @@
 		';
 	}
 	?>
-
