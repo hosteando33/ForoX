93664443553161847b0c4331222aeb9a2a2d6ca8
Dalbizu <70915352+Dalbizu@users.noreply.github.com>
2021-08-26 17:43:04 +0200

    Update forox.sql

--- forox.sql
+++ forox.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2021 a las 12:47:11
--- Versión del servidor: 10.4.10-MariaDB
--- Versión de PHP: 7.2.25
+-- Tiempo de generación: 26-08-2021 a las 17:40:26
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -39,8 +38,11 @@ CREATE TABLE `asignaturas` (
 --
 
 INSERT INTO `asignaturas` (`id_asignatura`, `nombre_asignatura`, `descripcion_asignatura`) VALUES
-(1, 'matemáticas', 'pues eso, todo y más'),
-(2, 'química', '');
+(1, 'Matemáticas', 'pues eso, todo y más'),
+(2, 'Química', ''),
+(3, 'Física', ''),
+(4, 'Lengua Castellana', ''),
+(5, 'Inglés', '');
 
 -- --------------------------------------------------------
 
@@ -61,12 +63,14 @@ CREATE TABLE `comentarios` (
 --
 
 INSERT INTO `comentarios` (`id_comentario`, `id_usuario`, `titulo_comentario`, `texto_comentario`, `fecha_comentario`) VALUES
-(11, 1, 'coment1', 'ssdñlkfalskdjfñal\r\nslkdfjñalksdj\r\nlksajdfñlkajs', '0000-00-00'),
-(12, 1, 'coment1', 'ssdñlkfalskdjfñal\r\nslkdfjñalksdj\r\nlksajdfñlkajs', '0000-00-00'),
-(13, 1, 'coment2', 'ssdñlkfalskdjfñal\r\nslkdfjñalksdj\r\nlksajdfñlkajs', '2021-06-22'),
-(14, 1, 'sasdasd', 'sadasdasd', '2021-06-22'),
-(15, 1, 'caca', 'me cago en tu sombra, profe', '2021-06-22'),
-(16, 1, 'segundo comengtario', 'asjdfñlaksjdfñlajdfñlkajñs\r\nlñkasjfdñlaksjdfñaskl\r\nañlkjdfñlkajdfñlkasdjf', '2021-06-23');
+(27, 32, 'Cuanto es 2 + 2', 'Ez mu difisil', '2021-08-20'),
+(28, 1, 'Mas mates', 'Mas mates', '2021-08-23'),
+(29, 1, 'Otra Pregunta mate', 'Pregunta de prueba mate', '2021-08-24'),
+(30, 1, 'Prueba con quínmica', 'Pregunta de prueba con quimica', '2021-08-24'),
+(31, 1, 'No se de fisica', 'Pregunta rpueba fisica', '2021-08-24'),
+(32, 1, 'Como hablar español', 'Solo se ingles', '2021-08-24'),
+(33, 1, 'Its very diuficult t', 'NI para de ingles', '2021-08-24'),
+(34, 1, 'Depilación', 'Como se depilan las ingles?\r\nGracias de antepierna', '2021-08-26');
 
 -- --------------------------------------------------------
 
@@ -75,9 +79,24 @@ INSERT INTO `comentarios` (`id_comentario`, `id_usuario`, `titulo_comentario`, `
 --
 
 CREATE TABLE `comentarios_asignaturas` (
+  `id_comentarios_asignaturas` int(4) NOT NULL,
   `id_comentario` int(6) NOT NULL,
   `id_asignatura` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `comentarios_asignaturas`
+--
+
+INSERT INTO `comentarios_asignaturas` (`id_comentarios_asignaturas`, `id_comentario`, `id_asignatura`) VALUES
+(5, 27, 1),
+(6, 28, 1),
+(7, 29, 1),
+(8, 30, 2),
+(9, 31, 3),
+(10, 32, 4),
+(11, 33, 5),
+(12, 34, 5);
 
 -- --------------------------------------------------------
 
@@ -130,7 +149,43 @@ CREATE TABLE `institutos` (
 
 INSERT INTO `institutos` (`id_instituto`, `nombre_instituto`) VALUES
 (1, 'Monte das Moas'),
-(2, 'Santa Maria del Mar');
+(2, 'Santa Maria del Mar'),
+(3, 'Monelos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE `respuestas` (
+  `id_respuesta` int(6) NOT NULL,
+  `texto_respuesta` text CHARACTER SET ascii NOT NULL,
+  `id_usuario` int(3) NOT NULL,
+  `id_comentario` int(6) NOT NULL,
+  `fecha_respuesta` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `respuestas`
+--
+
+INSERT INTO `respuestas` (`id_respuesta`, `texto_respuesta`, `id_usuario`, `id_comentario`, `fecha_respuesta`) VALUES
+(1, 'asdfasdfasdf', 1, 11, '2021-08-19'),
+(2, 'Claro que si guapi', 7, 15, '2021-08-19'),
+(3, 'De la vaca, supongo', 7, 15, '2021-08-20'),
+(4, 'Mientras sea en mi sombra...', 7, 15, '2021-08-20'),
+(5, 'asjdhfljas?ldfkj?lasdf', 32, 11, '2021-08-20'),
+(6, 'Ze lo que se siente.\r\nHe buscao en google y dece q es 4', 1, 27, '2021-08-25'),
+(7, 'TO esto. Busca a rajoy, mu buen nivel ', 7, 33, '2021-08-25'),
+(8, 'Pregunta algo quilloo', 7, 28, '2021-08-25'),
+(9, 'REspuesta de prueba mate\r\n', 7, 29, '2021-08-25'),
+(10, 'Respuesta de prueba quimica\r\n', 7, 30, '2021-08-25'),
+(11, 'REspuesta prueba fisica', 7, 31, '2021-08-25'),
+(12, 'Pues no se te da mal', 7, 32, '2021-08-25'),
+(13, 'You do it very well\r\n', 7, 32, '2021-08-25'),
+(14, 'Con mucho dolor, bonico', 33, 34, '2021-08-26'),
+(15, 'Nooo, eso dice mi novia', 33, 34, '2021-08-26');
 
 -- --------------------------------------------------------
 
@@ -154,7 +209,10 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id_usuario`, `id_instituto`, `nombre_usuario`, `apellido_usuario`, `email_usuario`, `password_usuario`) VALUES
 (1, 1, 'pedro', 'jimeno', 'jimeno@casita.com', '1234'),
 (2, 2, 'Pacheco', 'El cojo', 'sordoelultimo@catmail.com', 'ladesiempre'),
-(3, 1, 'Julio', 'Escobar', 'hipopotamo@catmail.com', '1234');
+(3, 1, 'Julio', 'Escobar', 'hipopotamo@catmail.com', '1234'),
+(7, 1, 'david', 'roibal', 'david@yo.com', '1234'),
+(32, 3, 'Oly', 'Guapa', 'oly@guapa', '1234'),
+(33, 3, 'Laila', 'Di', 'laila@mail.com', '1234');
 
 --
 -- Índices para tablas volcadas
@@ -173,6 +231,12 @@ ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`id_comentario`);
 
 --
+-- Indices de la tabla `comentarios_asignaturas`
+--
+ALTER TABLE `comentarios_asignaturas`
+  ADD PRIMARY KEY (`id_comentarios_asignaturas`);
+
+--
 -- Indices de la tabla `etiquetas`
 --
 ALTER TABLE `etiquetas`
@@ -183,6 +247,12 @@ ALTER TABLE `etiquetas`
 --
 ALTER TABLE `institutos`
   ADD PRIMARY KEY (`id_instituto`);
+
+--
+-- Indices de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD PRIMARY KEY (`id_respuesta`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -198,13 +268,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `asignaturas`
 --
 ALTER TABLE `asignaturas`
-  MODIFY `id_asignatura` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_asignatura` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id_comentario` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_comentario` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios_asignaturas`
+--
+ALTER TABLE `comentarios_asignaturas`
+  MODIFY `id_comentarios_asignaturas` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `etiquetas`
@@ -216,13 +292,19 @@ ALTER TABLE `etiquetas`
 -- AUTO_INCREMENT de la tabla `institutos`
 --
 ALTER TABLE `institutos`
-  MODIFY `id_instituto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_instituto` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  MODIFY `id_respuesta` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
